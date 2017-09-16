f93d4c1c4271bd5af028323e1e6c475b0b971599
suboklang <github224427>
2017-09-16 02:47:13 -0700

    006 - Initialising models

---
+++ src/models/users/user.py
@@ -0,0 +1,7 @@
+class User():
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return "<User {}>".format(self.email)
