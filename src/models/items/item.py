7f4686305eefdcb5a8a9fb83812394f1942bef28
suboklang <github224427>
2017-09-17 03:06:30 -0700

    016 - scrape item price

--- src/models/items/item.py
+++ src/models/items/item.py
@@ -1,8 +1,26 @@
+from bs4 import BeautifulSoup
+import requests
+import re
 class Item():
-    def __init__(self, name, price, url):
+    def __init__(self, name, price, store):
         self.name = name
-        self.price = price
         self.url = url
+        self.store = store
+        tag_name = store.get_tag_name()
+        query = store.query()
+        self.price = self.load_price(tag_name, query)
 
     def __repr__(self):
         return "<Item {} with URL {}>".format(self.name, self.url)
+
+    def load_price(self, tag_name, query):
+        request = requests.get(self.url)
+        content = request.content
+        soup = BeautifulSoup(content, "html.parser")
+        element = soup.find(tag_name, query)
+        string_price = element.text.strip()
+
+        pattern = re.compile("(\d+.\d+)")
+        match = pattern.search(string_price)
+
+        return match.group()
--- src/models/items/views.py
+++ src/models/items/views.py
@@ -6,10 +6,3 @@ item_blueprint = Blueprint('items', __name__)
 def item_page(name):
     pass
 
-@item_blueprint.route('/load')
-def load_item():
-    """
-    Loads an item's data using their store & return JSON
-    :return:
-    """
-    pass
